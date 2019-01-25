14a5f7c9e07393023a6723ed242e2016cec7834f
Nicolas VEDRINE <vedrinen@gmail.com>
2019-01-24 20:06:53 -0400

    Initial Commit

---
+++ iTunes Exporter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iTunes Exporter
+//
+//  Created by Developer on 24/01/2019.
+//  Copyright © 2019 Nicolas VEDRINE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

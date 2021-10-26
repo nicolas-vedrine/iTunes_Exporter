c67cd80a735aba95192ca51ec57a24a7c7ff72bd
Nicolas VEDRINE <vedrinen@gmail.com>
2021-10-26 14:55:10 -0400

    Routine de test pour ajout auto de tracks en mode debug.

---
+++ iTunes Exporter/Config/AppConfig.swift
@@ -0,0 +1,11 @@
+//
+//  AppConfig.swift
+//  iTunes Exporter
+//
+//  Created by Developer on 26/10/2021.
+//  Copyright © 2021 Nicolas VEDRINE. All rights reserved.
+//
+
+import Foundation
+
+var IS_DEBUG_MODE: Bool = true
